d9455d3d47d84d41bd47993a533a9c082670cff1
Doug Graham <dougg76@gmail.com>
2018-02-18 11:23:42 -0800

    cleansed strings

--- uninstall_application.ps1
+++ uninstall_application.ps1
@@ -1,4 +1,4 @@
-New-EventLog -LogName Application -Source "MME" -ErrorAction SilentlyContinue
+New-EventLog -LogName Application -Source "LOGNAME" -ErrorAction SilentlyContinue
 
 # Check to see if script is running on a server.
 $osName = Get-ComputerInfo -Property OsName
@@ -10,10 +10,10 @@ try {
     $application = Get-WmiObject Win32_Product -filter "Name='UnwantedProgram'"
     if ($application) {
         $application.Uninstall()
-        Write-EventLog -LogName Application -Source "MME" -Message "Application Removed" -EventId 0 -EntryType Information
+        Write-EventLog -LogName Application -Source "LOGNAME" -Message "Application Removed" -EventId 0 -EntryType Information
     } else {
-        Write-EventLog -LogName Application -Source "MME" -Message "Application not found" -EventId 1 -EntryType Information
+        Write-EventLog -LogName Application -Source "LOGNAME" -Message "Application not found" -EventId 1 -EntryType Information
     }
 } catch {
-    Write-EventLog -LogName Application -Source "MME" -Message "Unable to uninstall applcation`n $_" -EventId 2 -EntryType Error
+    Write-EventLog -LogName Application -Source "LOGNAME" -Message "Unable to uninstall applcation`n $_" -EventId 2 -EntryType Error
 }
